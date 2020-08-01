33d51ebd85581ce9cccddc5c43dd11a1ea21e14e
LKhoon <gnsdlrl@daum.net>
2020-08-01 20:46:05 +0900

    saturday final

--- rush00_saturdaynight/rush00.c
+++ rush00_saturdaynight/rush00.c
@@ -6,7 +6,7 @@
 /*   By: kilee <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/01 15:38:37 by kilee             #+#    #+#             */
-/*   Updated: 2020/08/01 19:15:22 by kilee            ###   ########.fr       */
+/*   Updated: 2020/08/01 20:38:56 by kilee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,13 @@ void		ft_putchar(char c);
 void		make_base(int x, int *c_x, char *sbl)
 {
 	while (*c_x < x)
-	{	if (*c_x == x-1)
-		{
-			ft_putchar(sbl[2]);
-			ft_putchar('\n');
-		}	
-		else if (*c_x == 0)
+	{	
+		if (*c_x == 0)
 			ft_putchar(sbl[0]);
 		else if (*c_x > 0 && *c_x < x-1)
 			ft_putchar(sbl[1]);
+		else if (*c_x == x-1)
+			ft_putchar(sbl[2]);
 		++*c_x;
 	}
 }
@@ -42,12 +40,14 @@ void 		rush(int x, int y)
 		if (c_y == 0 | c_y == y-1)
 		{
 			make_base(x, &c_x, "o-o");
+			ft_putchar('\n');
 			c_x = 0;
 			c_y++;
 		}
 		else
 		{
 			make_base(x, &c_x, "| |");
+			ft_putchar('\n');
 			c_x = 0;
 			c_y++;
 		}
--- rush00_saturdaynight/rush01.c
+++ rush00_saturdaynight/rush01.c
@@ -6,7 +6,7 @@
 /*   By: kilee <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/01 15:38:37 by kilee             #+#    #+#             */
-/*   Updated: 2020/08/01 20:33:54 by kilee            ###   ########.fr       */
+/*   Updated: 2020/08/01 20:43:21 by kilee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,57 +14,50 @@
 
 void		ft_putchar(char c);
 
-void		make_base(int y, int *c_y, char *sbl)
+void		make_base(int x, int *c_x, char *sbl)
 {
-	while (*c_y < y)
-	{
-		if (*c_y == 0)
+	while (*c_x < x)
+	{	
+		if (*c_x == 0)
 			ft_putchar(sbl[0]);
-		else if (*c_y > 0 && *c_y < y-1)
+		else if (*c_x > 0 && *c_x < x-1)
 			ft_putchar(sbl[1]);
-		else if (*c_y == y-1)
-		{
+		else if (*c_x == x-1)
 			ft_putchar(sbl[2]);
-		}
-		++*c_y;
+		++*c_x;
 	}
 }
-
-void		build_base(int x, int y)
+	
+void 		rush(int x, int y)
 {
 	int c_x;
 	int c_y;
 
 	c_x = 0;
 	c_y = 0;
-	while (c_x < x)
+	while (c_y < y)
 	{
-		if (c_x == 0)
+		if (c_y == 0)
 		{
-			make_base(y, &c_y, "/*\\");
+			make_base(x, &c_x, "/*\\");
 			ft_putchar('\n');
-			c_y = 0;
-			c_x++;
+			c_x = 0;
+			c_y++;
 		}
-		else if (c_x == x-1)
+		else if (c_y == y-1)
 		{
-			make_base(y, &c_y, "\\*/");
+			make_base(x, &c_x, "\\*/");
 			ft_putchar('\n');
-			c_y = 0;
-			c_x++;
+			c_x = 0;
+			c_y++;
 		}
 		else
 		{
-			make_base(y, &c_y, "* *");
+			make_base(x, &c_x, "* *");
 			ft_putchar('\n');
-			c_y = 0;
-			c_x++;
+			c_x = 0;
+			c_y++;
 		}
 	}
-}
-	
-void 		rush(int x, int y)
-{
-	build_base(x,y);
 }
 
