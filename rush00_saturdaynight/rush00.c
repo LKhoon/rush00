fe71e0f6aacdc572b4cc81acc4543734592c70f0
LKhoon <gnsdlrl@daum.net>
2020-08-01 20:56:23 +0900

    0 square filter

--- rush00_saturdaynight/rush00.c
+++ rush00_saturdaynight/rush00.c
@@ -6,7 +6,7 @@
 /*   By: kilee <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/01 15:38:37 by kilee             #+#    #+#             */
-/*   Updated: 2020/08/01 20:38:56 by kilee            ###   ########.fr       */
+/*   Updated: 2020/08/01 20:55:43 by kilee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void		ft_putchar(char c);
 
-void		make_base(int x, int *c_x, char *sbl)
+void		make_line(int x, int *c_x, char *sbl)
 {
 	while (*c_x < x)
 	{	
@@ -28,7 +28,7 @@ void		make_base(int x, int *c_x, char *sbl)
 	}
 }
 	
-void 		rush(int x, int y)
+void 		make_square(int x, int y)
 {
 	int c_x;
 	int c_y;
@@ -39,14 +39,14 @@ void 		rush(int x, int y)
 	{
 		if (c_y == 0 | c_y == y-1)
 		{
-			make_base(x, &c_x, "o-o");
+			make_line(x, &c_x, "o-o");
 			ft_putchar('\n');
 			c_x = 0;
 			c_y++;
 		}
 		else
 		{
-			make_base(x, &c_x, "| |");
+			make_line(x, &c_x, "| |");
 			ft_putchar('\n');
 			c_x = 0;
 			c_y++;
@@ -54,3 +54,10 @@ void 		rush(int x, int y)
 	}
 }
 
+void		rush(int x, int y)
+{
+	if (x == 0 | y == 0)
+		ft_putchar('\n');
+	else
+		make_square(x, y);
+}
